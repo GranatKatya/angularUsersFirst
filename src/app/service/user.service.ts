d53042ce0de384bcdc5f143f802fdc2207b2dd9f
Katya Granat <k.granat2017@.com>
2019-10-20 19:16:31 +0300

    second

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -12,68 +12,29 @@ export class UserService {
   //constructor(private http: HttpClient) { }
  // baseUrl: string = 'http://localhost:8080/user-portal/users';
 
-  getUsers() {
-   // return this.http.get<User[]>(this.baseUrl);
-   this.users = [];
-  //  var values = JSON.parse(localStorage.getItem("adduser"));
- //  console.log("localst");
-//  console.log(localStorage);
-// var  local = localStorage;
-
- //console.log(localStorage); //result show in below  
-
- //const prices = local.map(data => data.productPrice); 
- //console.log(prices); // array of all the prices
-
-    for (var key in localStorage) { 
-     //  console.log( key);
+  getUsers() {  
+   this.users = []; 
+    for (var key in localStorage) {   
       if (key.includes("adduser")){
-        var value = JSON.parse(localStorage.getItem(key));
-      //  console.log(value);
-        
-        // this.user.id= value.id,
-        // this.user.email= value.email,
-        // this.user.firstName = value.firstName,
-        // this.user.lastName = value.lastName
-        // console.log(this.user);
+        var value = JSON.parse(localStorage.getItem(key));    
         this.users.push(value );
       }
-    }
-
-    // localStorage.fforEach(values, function(item) {
-
-    //   console.log(item);
-    //   if(){
-    //     this.users.push(item);
-    //   }    
-    // });
-    // console.log(this.users);
-    //console.log("getUsers");
-   
-    // values.forEach(values, function(item) {
-    //    console.log(item);
-    //    this.users.push(item);
-    //  });
+    }   
     return this.users;
   }
 
   getUserById(id: number) {
   //  return this.http.get<User>(this.baseUrl + '/' + id);
-
-  var value = JSON.parse(localStorage.getItem("adduser"+ id)); 
-
-  
+  var value = JSON.parse(localStorage.getItem("adduser"+ id));   
     return value;
   }
 
-  createUser(user: User) {
-   
-   this.id =  parseInt(localStorage.getItem("id"));
-    
+  createUser(user: User) {   
+   this.id =  parseInt(localStorage.getItem("id"));    
     user.id = this.id;
    // return this.http.post(this.baseUrl, user);
      localStorage.setItem("adduser"+ user.id, JSON.stringify(user)); //user.toString());
-     console.log( user);
+    // console.log( user);
      this.id++;
      localStorage.setItem("id", this.id.toString());
    //  return Response.;
@@ -87,9 +48,7 @@ export class UserService {
   //  return this.http.put(this.baseUrl + '/' + user.id, user);
   }
 
-  deleteUser(id: number) {
- 
-      localStorage.removeItem("adduser"+id);
-   
+  deleteUser(id: number) { 
+      localStorage.removeItem("adduser"+id);   
   }
 }
